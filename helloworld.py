935dcb3702cf78ab074e7842941e25eeedd0fd01
Marcin Kulik <marcinkulik@icloud.com>
2024-07-16 18:19:22 +0100

    where is homework

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,4 @@
 print("Hello World!")
 print("Thank you for invitation")
 print("Another change, first change with GitHub for Desktop")
+print("How is your homework going? Where is a new branch with changes?:)")
